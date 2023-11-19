af86f3d229b11b710f847ef5be12209d6dbe2c4c
Aristotele Tufano <91723143+ariStridely@users.noreply.github.com>
2023-11-19 22:51:10 +0100

    Add roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -42,4 +43,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    /**
+     * Returns wether the user has the given role or not.
+     *
+     * @param string $roleName
+     * @return boolean
+     */
+    public function hasRole(string $roleName): bool
+    {
+        return $this->roles()->pluck('name')->contains($roleName);
+    }
 }
