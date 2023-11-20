569d6ff3c7f66cb5c9ec66f0890de2c551ae13cc
Aristotele Tufano <91723143+ariStridely@users.noreply.github.com>
2023-11-20 01:25:33 +0100

    Tweak seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,8 +5,11 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Role;
+use App\Models\Tour;
 use App\Models\User;
+use App\Models\Travel;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,8 +23,24 @@ class DatabaseSeeder extends Seeder
 
         $admin = User::factory()->create([
             'email' => 'test@mail.com',
-        ])->create();
+        ]);
 
         $admin->roles()->attach($adminRole);
+
+        foreach (range(1, 10) as $i) {
+            $travel = Travel::factory(['numberOfDays' => rand(5, 9)])->create();
+            $travelTours = Tour::factory()
+                ->for($travel)
+                ->count(4)
+                ->state(
+                    new Sequence(
+                        ['price' => rand(500, 3000) * 100, 'startingDate' => now()->addDays(10), 'endingDate' => now()->addDays(10 + $travel->numberOfDays)],
+                        ['price' => rand(500, 3000) * 100, 'startingDate' => now()->addDays(20), 'endingDate' => now()->addDays(20 + $travel->numberOfDays)],
+                        ['price' => rand(500, 3000) * 100, 'startingDate' => now()->addDays(30), 'endingDate' => now()->addDays(30 + $travel->numberOfDays)],
+                        ['price' => rand(500, 3000) * 100, 'startingDate' => now()->addDays(40), 'endingDate' => now()->addDays(40 + $travel->numberOfDays)],
+                    )
+                )
+                ->create();
+        }
     }
 }
